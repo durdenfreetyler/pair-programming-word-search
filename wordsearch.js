8430dc5a0098dd1be47bd5e5ce0b3720cace87e5
Abe <abrahamrodgers@gmail.com>
2023-01-18 15:44:56 -0800

    Compass exercise paired programming word search.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
 
-module.exports = wordSearch
+  const verticalJoin = letters[0].map((_, colIndex) =>
+    letters.map((row) => row[colIndex]).join("")
+  );
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+const result = wordSearch(
+  [
+    ["A", "W", "C", "F", "Q", "U", "A", "L"],
+    ["S", "E", "I", "N", "F", "E", "L", "D"],
+    ["C", "F", "C", "F", "Q", "U", "A", "L"],
+    ["A", "M", "J", "T", "E", "V", "R", "G"],
+    ["T", "H", "C", "S", "Y", "E", "R", "L"],
+    ["B", "F", "R", "E", "N", "E", "Y", "B"],
+    ["U", "B", "T", "W", "A", "P", "A", "I"],
+    ["O", "D", "C", "A", "K", "U", "A", "S"],
+    ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+  ],
+  "CAT"
+);
+
+module.exports = wordSearch;
